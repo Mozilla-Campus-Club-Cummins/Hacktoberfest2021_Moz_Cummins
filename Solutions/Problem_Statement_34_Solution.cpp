dc9bfffcc29998dac1b75fb30ed00afe92d6ceb3
ishwari20 <56400099+ishwari20@users.noreply.github.com>
2021-10-02 14:24:10 +0530

    Update Problem_Statement_34_Solution.cpp

--- Solutions/Problem_Statement_34_Solution.cpp
+++ Solutions/Problem_Statement_34_Solution.cpp
@@ -16,30 +16,26 @@ int main()
 	//bi is the number of oranges in the i-th gift
 	//here n is number of gifts
     	int a[100], b[100], n;
-	int t;
-	cin >> t;
-    	//input the number of testcases
-	while(t--)
+
+	cin >> n;
+	int minOfA = INT_MAX, minOfB = INT_MAX;
+	long long minMoves = 0;
+	for(int i = 1; i <= n; i++)
 	{
-		cin >> n;
-		int minOfA = INT_MAX, minOfB = INT_MAX;
-		long long minMoves = 0;
-		for(int i = 1; i <= n; i++)
-		{
-			cin >> a[i];
-			minOfA = min(minOfA, a[i]);
-		}	
-		for(int i = 1; i <= n; i++)
-		{
-			cin >> b[i];
-			minOfB = min(minOfB, b[i]);
-		}
-		for(int i = 1; i <= n; i++)
-		{
-			minMoves += 1ll * max(a[i] - minOfA, b[i] - minOfB);
-		}
-		cout << minMoves << endl;
+		cin >> a[i];
+		minOfA = min(minOfA, a[i]);
+	}	
+	for(int i = 1; i <= n; i++)
+	{
+		cin >> b[i];
+		minOfB = min(minOfB, b[i]);
 	}
+	for(int i = 1; i <= n; i++)
+	{
+		minMoves += 1ll * max(a[i] - minOfA, b[i] - minOfB);
+	}
+	cout << minMoves << endl;
+
 	return 0;
 }
 
@@ -47,28 +43,11 @@ int main()
 /*
 
 Sample Input:
-5
 3
 3 5 6
 3 2 3
-5
-1 2 3 4 5
-5 4 3 2 1
-3
-1 1 1
-2 2 2
-6
-1 1000000000 1000000000 1000000000 1000000000 1000000000
-1 1 1 1 1 1
-3
-10 12 8
-7 5 4
 
 Sample Output:
 6
-16
-0
-4999999995
-7
 
 */
