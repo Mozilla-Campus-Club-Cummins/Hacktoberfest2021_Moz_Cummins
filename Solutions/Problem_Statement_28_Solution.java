d2ea8a34587d7b2e6ff01ed51a959aa002b59770
Vaishnavi <vaishnavimarkunde7001@gmail.com>
2021-10-03 12:38:49 +0530

    Updated solution for issue 12

--- Solutions/Problem_Statement_28_Solution.java
+++ Solutions/Problem_Statement_28_Solution.java
@@ -1,15 +1,38 @@
+/*Problem_Statement_28 */
+/*
+ A friend of mine takes the sequence of all numbers from 1 to n (where n > 0).
+Within that sequence, she chooses two numbers, a and b.
+She says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
+Given a number n, could you tell me the numbers she excluded from the sequence?
+The function takes the parameter: n (n is always strictly greater than 0) and returns an array or a string
+(depending on the language) of the form:
+
+[(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
+with all (a, b) which are the possible removed numbers in the sequence 1 to n.
+[(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ... will be sorted in increasing order of the ""a"".
+It happens that there are several possible (a, b). The function returns an empty array 
+(or an empty string) if no possible numbers are found which will prove that my friend has not told the truth!
+
+ * */
+
+
 package mozilla;
 
-import java.util.ArrayList;
+import java.util.*;
 
-public class Problem_28 {
+public class Problem_Statement_28_Solution {
 
 	public static void main(String[] args) {
-		int n =10;
-		int m=20;
+		Scanner sc = new Scanner(System.in);
+		int n=-1;
+		while(n<=0) {
+		//Taking n from user 
+		System.out.println("Please enter a integer n greater than 0 : ");
+		n =sc.nextInt();
+		}
        //Calling the function- excludedNumbers which returns an array of excluded numbers [a,b] for any input n
 		System.out.println(excludedNumbers(n));
-		System.out.println(excludedNumbers(m));
+		
 
 	}
 	
@@ -45,8 +68,12 @@ public class Problem_28 {
 
 /*/************************      OUTPUT             **************************//*
 
+Please enter a integer n greater than 0 : 
+-3
+Please enter a integer n greater than 0 : 
+10
 [[6, 7], [7, 6]]
-[]
+
 
 
 */
