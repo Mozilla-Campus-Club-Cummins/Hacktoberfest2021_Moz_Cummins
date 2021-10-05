bb7f820871e4aa528d4e17aa0367b2a7c7588094
Aditi-Bornare <55871869+Aditi-Bornare@users.noreply.github.com>
2021-10-05 11:59:03 +0530

    Updated the Solution to Problem Statement 55

--- Solutions/Problem_Statement_55_Solution.py
+++ Solutions/Problem_Statement_55_Solution.py
@@ -28,47 +28,96 @@
 # 1: q2 -> q2
 # 0: q2 -> q3
 # We end in q3 which is not our accept state, so we return false.
-def Automation(inputString):
-    q1="q1"
-    q2="q2"
-    q3="q3"
-    currentState=q1
-    for i in inputString:
-        # q1 moves to q2 when given a 1, and stays at q1 when given a 0
-        if currentState==q1:
-            if i=='0':
-                nextState=q1
-            elif i=='1':
-                nextState=q2
-        # q2 moves to q3 when given a 0, and stays at q2 when given a 1
-        elif currentState==q2:
-            if i=='0':
-                nextState=q3
-            elif i=='1':
-                nextState=q2
-        # q3 moves to q2 when given a 0 or 1
-        elif currentState==q3:
-            nextState=q2
-        currentState=nextState
-    if currentState==q2:
-        return True
-    else:
-        return False
 
-# Regular Expression
-import re
+class Automaton:
 
-# Accept inputString from user
-inputString=input("Enter your input string: ")
+    # constructor
+    def __init__(self):
 
+        # Since initial state is q1, set currentState to q1
+        self.currentState="q1"
+        self.nextState=""
+
+    def read_commands(self,inputString):
+        for i in inputString:
+
+            # q1 moves to q2 when given a 1, and stays at q1 when given a 0
+            if self.currentState=="q1":
+                if i=='1':
+                    self.nextState="q2"
+
+            # q2 moves to q3 when given a 0, and stays at q2 when given a 1
+            elif self.currentState=="q2":
+                if i=='0':
+                    self.nextState="q3"
+
+            # q3 moves to q2 when given a 0 or 1
+            elif self.currentState=="q3":
+                self.nextState="q2"
+
+            self.currentState=self.nextState
+
+        # if currentState is a final state then return True
+        if self.currentState=="q2":
+            return True
+        # else return False
+        else:
+            return False
+
+# Accept inputString in the form of list from user
+inputString=eval(input("Enter your input string: "))
+
+# Convert it into a set for input validation
+set1=set(inputString)
+set2={"0","1"}
+
+# difference returns elements present in the first set and not
+# in the second set. So if the inputString is valid then
+# the difference of set1 and set2 will be an empty set
 # To validate that the inputString is made only of 0 and 1
-fm=re.fullmatch("[0-1]+",inputString)
+if len(set1.difference(set2))==0:
 
-# If the inputString is valid then call Automation
-if fm!=None:
-    print(Automation(inputString))
+    # Create instance of Automaton class
+    instance=Automaton()
 
-# Otherwise give a message to user saying inputString is invalid
+    # Call method read_commands using instance
+    print(instance.read_commands(inputString))
+
+# If inputString is not valid then give a message to user saying inputString is invalid
 else:
     print("Invalid Input String")
-    print("Please enter a valid input string made of only 0 and 1")
+    print("Please enter a valid input string")
+
+
+# Sample Inputs and Outputs
+
+# Enter your input string: ["1","0","0","1","0"]
+# False
+#
+# Enter your input string: ["1","0","1"]
+# True
+#
+# Enter your input string: ["1","0","1","2"]
+# Invalid Input String
+# Please enter a valid input string
+#
+# Enter your input string: ["2","1"]
+# Invalid Input String
+# Please enter a valid input string
+#
+# Enter your input string: ["1"]
+# True
+#
+# Enter your input string: ["0"]
+# False
+#
+# Enter your input string: ["1","a"]
+# Invalid Input String
+# Please enter a valid input string
+#
+# Enter your input string: []
+# False
+#
+# Enter your input string: [1,0]
+# Invalid Input String
+# Please enter a valid input string
